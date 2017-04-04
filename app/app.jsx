58bab00deb3364e9d60628b6d85b95a6d8401972
roblovelett <rlovelett@gmail.com>
2017-04-04 15:41:59 -0500

    created roster, an array of objects containing name, score, and id, added map method of players to return above, added application proptype of players to be an array of objects with a shape of name, score, id

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,29 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+var roster = [
+    {
+        name: "Billy 01",
+        score: 23,
+        id: 1
+    },
+    {
+        name: "Billy 02",
+        score: 56,
+        id: 2
+    },
+    {
+        name: "Billy 03",
+        score: 34,
+        id: 3
+    },
+    {
+        name: "Billy 04",
+        score: 45,
+        id: 4
+    }
+];
+
 function Header(props) {
     return (
         <div className="header">
@@ -54,19 +77,27 @@ function Application(props) {
         <div className="scoreboard">
             <Header title={props.title} />
             <div className="players">
-                <Player name="Jimbo Jones" score={31} />
-                <Player name="Maurice LePen" score={28} />
+                {props.players.map(function(player){ //map method takes a function itself, takes each player inside our roster array
+                    return (
+                        <Player name={player.name} score={player.score} key={player.id} /> //key property is required on components rendered in a loop
+                    );
+                })}
             </div>
         </div> 
     );
 };
 
 Application.propTypes = { 
-    title: React.PropTypes.string
+    title: React.PropTypes.string,
+    players: React.PropTypes.arrayOf(React.PropTypes.shape({ //shape defines the structure and proptypes of the object
+        name: React.PropTypes.string.isRequired,
+        score: React.PropTypes.number.isRequired,
+        id: React.PropTypes.number.isRequired
+    })).isRequired
 };
 
 Application.defaultProps = {
     title: "Scoreboard"
 };
 
-ReactDOM.render(<Application title="My Scoreboard" />, document.getElementById('container'));
+ReactDOM.render(<Application title="My Scoreboard" players={roster} />, document.getElementById('container'));
