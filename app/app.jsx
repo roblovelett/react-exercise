8419d83f4df4170f84a83bbf7301999ad4c50295
roblovelett <rlovelett@gmail.com>
2017-04-04 15:11:07 -0500

    Created Player and Counter components

--- app/app.jsx
+++ app/app.jsx
@@ -2,47 +2,67 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+function Header(props) {
+    return (
+        <div className="header">
+            <h1>{props.title}</h1>
+        </div>
+    );
+};
+
+Header.propTypes = { 
+    title: React.PropTypes.string.isRequired
+};
+
+function Player(props) {
+    return (
+        <div className="player">
+            <div className="player-name">
+                {props.name}
+            </div>
+            <Counter score={props.score} />
+        </div>
+    );
+};
+
+Player.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired
+};
+
+function Counter(props) {
+    return (
+        <div className="player-score">
+            <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> {props.score} </div>
+                <button className="counter-action increment"> + </button>
+            </div>
+        </div>
+    );
+};
+
+Counter.propTypes = {
+    score: React.PropTypes.number.isRequired
+};
+
 function Application(props) {
     return (
         //jsx is not strict html and cannot use 'class' to apply css styling. in
         //js class is already used. you can use a custom field do define a class
         //i.e. "className"
         <div className="scoreboard">
-            <div className="header">
-                <h1>{props.title}</h1>
-            </div>
+            <Header title={props.title} />
             <div className="players">
-                <div className="player">
-                    <div className="player-name">
-                        Jimbo Jones
-                    </div>
-                    <div className="player-score">
-                        <div className="counter">
-                            <button className="counter-action decrement"> - </button>
-                            <div className="counter-score"> 31 </div>
-                            <button className="counter-action increment"> + </button>
-                        </div>
-                    </div>
-                </div>
-                <div className="player">
-                    <div className="player-name">
-                        Maurice LePen
-                    </div>
-                    <div className="player-score">
-                        <div className="counter">
-                            <button className="counter-action decrement"> - </button>
-                            <div className="counter-score"> 28 </div>
-                            <button className="counter-action increment"> + </button>
-                        </div>
-                    </div>
-                </div>
+                <Player name="Jimbo Jones" score={31} />
+                <Player name="Maurice LePen" score={28} />
             </div>
         </div> 
     );
 };
 
 Application.propTypes = { 
-    title: React.PropTypes.string,
+    title: React.PropTypes.string
 };
 
 Application.defaultProps = {
